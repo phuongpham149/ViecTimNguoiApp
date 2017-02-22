525ea56703448a7f7f8845ddfc19308b53928586
at-phuongpham <phuong.pham@asiantech.vn>
2017-02-22 09:19:01 +0700

    tao bai dang viec lam

--- app/src/main/java/com/example/phuong/viectimnguoiapp/fragments/NewsFragment.java
+++ app/src/main/java/com/example/phuong/viectimnguoiapp/fragments/NewsFragment.java
@@ -1,16 +1,43 @@
 package com.example.phuong.viectimnguoiapp.fragments;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.example.phuong.viectimnguoiapp.R;
+import com.example.phuong.viectimnguoiapp.adapters.NewsAdapter;
+import com.example.phuong.viectimnguoiapp.objects.NewItem;
 
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by phuong on 21/02/2017.
  */
 @EFragment(R.layout.fragment_news)
 public class NewsFragment extends BaseFragment {
+    @ViewById(R.id.recyclerViewNews)
+    RecyclerView mRecyclerViewNews;
+
+    private NewsAdapter mAdapter;
+    private List<NewItem> mNews;
+
     @Override
     void inits() {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        initsData();
+        mRecyclerViewNews.setLayoutManager(layoutManager);
+        mAdapter = new NewsAdapter(mNews, getContext());
+        mRecyclerViewNews.setAdapter(mAdapter);
+    }
 
+    public void initsData() {
+        mNews = new ArrayList<>();
+        NewItem newItem = new NewItem(new Date(), "Tìm thợ sửa mái nhà", "Làm vào ngày 23/2/2017", 1);
+        mNews.add(newItem);
+        mNews.add(newItem);
     }
 }
--- app/src/main/java/com/example/phuong/viectimnguoiapp/utils/Constant.java
+++ app/src/main/java/com/example/phuong/viectimnguoiapp/utils/Constant.java
@@ -7,5 +7,6 @@ package com.example.phuong.viectimnguoiapp.utils;
 public class Constant {
     public static final String DATA_NAME_USER_LOGIN = "user_login";
     public static final String NAME_USER_LOGIN = "username";
+    public static final String ID_USER_LOGIN = "idUser";
 
 }
